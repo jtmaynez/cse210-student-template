cd4291616fc10c3ada2b4540ba79d613d9b5f88c
Justin Maynes <may20012@byui.edu>
2025-04-23 22:56:43 -0600

    changed Readme and created first c# code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+
+// This is a program to  compute area of a circle
+// Get the radius of the circle from the user
+        console.Write("Please Enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        // compute the area
+        double area = Math.PI * radius * radius
+        Console.WriteLine($"Area of the circle: {area}");
+        int x = 3;
+        string s = "goodbye";
+        float f = 3.14F;
+        double d = 5.21;
+        long n = 25;
+        Console.WriteLine($"Hello, {s} Sandbox World! {x} {f} {d}");
     }
 }
