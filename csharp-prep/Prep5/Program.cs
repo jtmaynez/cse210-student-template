f221e39131344bdd2e9e6650e690911d8bb3a8be
Justin Maynes <may20012@byui.edu>
2025-04-30 09:37:19 -0600

    Finish prep 4 and 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       DisplayWelcomeMessage();
+       string name = Promptname();
+       int favNumber = PromptFavNumber();
+       int squareNumber = SquareNumber(favNumber);
+
+       DisplayResult(name, squareNumber);
     }
+static void DisplayWelcomeMessage()
+{
+    Console.WriteLine("Welcome to the program! ");
+}
+static string Promptname()
+{
+    Console.Write("Please Enter your name: ");
+    string name = Console.ReadLine();
+
+    return name;
+}
+
+static int PromptFavNumber()
+{
+Console.Write("Please enter your favorite number: ");
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+static int SquareNumber(int number)
+{
+int square = number * number;
+return square;
+}
+static void DisplayResult(string name, int square)
+{
+Console.WriteLine($"{name}, the square of your number is {square}!");
+}
+
 }
