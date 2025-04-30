7358a98e23e36f8badcbe974483beee866b0c91f
Justin Maynes <may20012@byui.edu>
2025-04-30 07:04:27 -0600

    Guess the number code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,101);
+    //     Console.Write("What is the magic number? ");
+    //   int magic = int.Parse(Console.ReadLine());
+    int guess = 0;
+     while (magic != guess)
+        {
+
+        Console.Write("What is your guess? ");
+         guess = int.Parse(Console.ReadLine());
+
+        if ( magic < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if ( magic > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else {
+            Console.WriteLine("You guessed it!");
+        }
+        }
     }
 }
