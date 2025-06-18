7ee7c588605583407a271ad831070b3045d68be4
Justin Maynes <may20012@byui.edu>
2025-06-18 07:45:27 -0600

    Shape activity done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,9 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Notice that the list is a list of "Shape" objects. That means
+        // you can put any Shape objects in there, and also, any object where
+        // the class inherits from Shape
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Green", 5, 7);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Blue", 5);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
-
-
-    
 }
