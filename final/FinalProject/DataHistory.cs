426907a34d203591ee59dd73c581758651cda3fc
Justin Maynes <may20012@byui.edu>
2025-07-12 23:16:28 -0600

    commit

--- final/FinalProject/DataHistory.cs
+++ final/FinalProject/DataHistory.cs
@@ -39,4 +39,6 @@ public class DataHistory
         month --; 
         return months[month];
 
- 
+    }
+
+}
--- final/FinalProject/Forecast.cs
+++ final/FinalProject/Forecast.cs
@@ -27,4 +27,34 @@ public abstract class Forecast
         int actual = month.HasValue
             ? _data.GetMonth(year, month.Value)
             : quarter.HasValue
-       
+                ? _data.GetQuarterly(year, quarter.Value)
+                : _data.GetYear(year);
+
+        return actual != 0 ? Math.Abs((double)(actual - forecast) / actual) : 0;
+    }
+    protected (int year, int quarter) GetPreviousQuarter(int year, int quarter, int stepsBack)
+//Tuple
+    {
+    int newQuarter = quarter - stepsBack;
+    while (newQuarter <= 0)
+    {
+        year--;
+        newQuarter += 4;
+    }
+    return (year, newQuarter);
+}
+protected (int year, int month) GetPreviousMonth(int year, int month, int stepsBack)
+{
+    int newMonth = month - stepsBack;
+    while (newMonth <= 0)
+    {
+        year--;
+        newMonth += 12;
+
+        if (year < 2015)
+            throw new ArgumentException("Cannot forecast before January 2015 — not enough historical data.");
+    }
+    return (year, newMonth);
+}
+
+}
